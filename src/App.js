56217801b4d00048b43a66a57d686848d8583236
bhag2027 <bhagyazz2027@gmail.com>
2024-09-10 18:40:57 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddUser from './Components/AddUser';
 
 function App() {
   return (
     <div>
-
+<BrowserRouter>
+<Routes>
+  <Route path='/reg' element={<AddUser/>}/>
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
